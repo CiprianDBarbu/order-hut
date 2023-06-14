c8ddf3b74c994c8810f106db18feadfee2ac12ca
Ciprian Barbu <barbu.ciprian.dragos@gmail.com>
2023-06-14 23:50:01 +0300

    Adds general styling. Hides menu tabs based on roles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Buffer } from 'buffer';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,16 @@ export class AppComponent implements OnInit {
   title = 'Order Hut';
 
   visible!: boolean;
-  isAdmin?: boolean = true;
+  isAdmin?: boolean;
+  isWaiter?: boolean;
+  userName!: string;
 
   ngOnInit(): void {
     // this.isAdmin = sessionStorage.getItem("token") === null ? false : true;
+    const token = sessionStorage.getItem("token")!;
+    this.userName = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString()).username;
+    this.isAdmin =  this.userName === "admin" ? true : false;
+    this.isWaiter =  this.userName === "waiter" ? true : false;
   }
 
   showDialog() {
